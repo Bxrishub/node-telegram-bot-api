0080ffeb11b1c420e70fa2709dc9edf637f6b5b4
Bxrishub <67127819+Bxrishub@users.noreply.github.com>
2020-08-31 00:31:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-/**
- * If running on Nodejs 5.x and below, we load the transpiled code.
- * Otherwise, we use the ES6 code.
- * We are deprecating support for Node.js v5.x and below.
- */
-const majorVersion = parseInt(process.versions.node.split('.')[0], 10);
-if (majorVersion <= 5) {
-  const deprecate = require('depd')('node-telegram-bot-api');
-  deprecate('Node.js v5.x and below will no longer be supported in the future');
-  module.exports = require('./lib/telegram');
-} else {
-  module.exports = require('./src/telegram');
-}
+$ mkdir telegram-bot-projem
+$ cd telegram-bot-projem
+$ yarn init
+yarn init v1.5.1
+question name (telegram-bot-projem): 
+question version (1.0.0): 
+question description: Telegram Bot Projem
+question entry point (index.js): 
+question repository url: 
+question author: benim
+question license (MIT): GPL-3.0
+question private: public
+success Saved package.json
+Done in 30.31s.
+$ yarn add node-telegram-bot-api
+yarn add v1.5.1
+info No lockfile found.
+[1/4] Resolving packages...
+[2/4] Fetching packages...
+[3/4] Linking dependencies...
+[4/4] Building fresh packages...
+success Saved lockfile.
+success Saved 82 new dependencies.
+info Direct dependencies
+└─ node-telegram-bot-api@0.30.0
+info All dependencies
+...
+Done in 3.25s.
+$ ls
+node_modules  package.json  yarn.lock
+$ touch index.js
